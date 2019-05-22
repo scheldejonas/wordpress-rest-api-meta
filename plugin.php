be7cc44d55fabb5f6f8960e0ffb48f5afa225b2b
Jonas Schelde <kontakt@jonasschelde.dk>
2019-05-22 23:07:05 +0200

    v1.0.0
    License

--- plugin.php
+++ plugin.php
@@ -6,6 +6,8 @@
  * Version: 1.0.0
  * Author URI: https://www.jonasschelde.dk
  * Plugin URI: https://github.com/scheldejonas/wordpress-rest-api-meta
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
 
